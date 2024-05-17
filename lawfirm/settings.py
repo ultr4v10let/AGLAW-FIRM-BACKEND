83ecda57f1983e0b8e6eacadc44862d55aefb741
AbdelRahman Romayh <abdelrahman.romayh@gmail.com>
2024-05-17 17:14:01 +0300

    added cors headers

--- lawfirm/settings.py
+++ lawfirm/settings.py
@@ -42,10 +42,12 @@ INSTALLED_APPS = [
     'teammembers',
     'legalservices',
     'news',
-    'clients'
+    'clients',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -137,3 +139,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+CORS_ALLOWED_ORIGINS = ['*']
