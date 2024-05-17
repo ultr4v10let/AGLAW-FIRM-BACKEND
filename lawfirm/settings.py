f458e8c2096d0e390d3073305ebb295fca8ea7a4
AbdelRahman Romayh <abdelrahman.romayh@gmail.com>
2024-05-17 17:31:57 +0300

    added cors settings

--- lawfirm/settings.py
+++ lawfirm/settings.py
@@ -141,4 +141,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = ['*']
+CORS_ALLOW_HEADERS = ['*']
+CORS_ALLOW_METHODS = ['*']
